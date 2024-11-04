2d66725cb4475c2fea3568e9605821cccab5fdb8
Ikbal Čaušević <ikbalcaus@gmail.com>
2024-11-04 18:18:37 +0100

    Added drawing with mouse

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ mp_draw = mp.solutions.drawing_utils
 mp_hand = mp.solutions.hands
 video = cv2.VideoCapture(0)
 
-width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+if video.isOpened():
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+else:
+    width = 640
+    height = 480
 canvas = np.ones((height, width, 3), dtype="uint8") * 255
-
-prev_x, prev_y = None, None
 
 root = tk.Tk()
 root.title("Hand Drawing Canvas")
@@ -28,6 +30,11 @@ canvas_label.pack()
 cursor_label = tk.Label(canvas_frame, width=1, height=1, bg="lightgrey")
 cursor_label.place(x=-20, y=-20)
 
+mouse_mode = True
+mouse_drawing = False
+mouse_erasing = False
+prev_x, prev_y = None, None
+
 def update_canvas():
     img = Image.fromarray(canvas)
     imgtk = ImageTk.PhotoImage(image=img)
@@ -36,17 +43,17 @@ def update_canvas():
     canvas_label.after(10, update_canvas)
 
 def video_stream():
-    global prev_x, prev_y, canvas
+    global canvas, mouse_mode, prev_x, prev_y
 
     with mp_hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
         while True:
-            ret, frame = video.read()
+            ret, frame = video.read()            
             frame = cv2.flip(frame, 1)
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             results = hands.process(rgb_frame)
 
-            if results.multi_hand_landmarks:
+            if results.multi_hand_landmarks and not mouse_mode:
                 hand_landmark = results.multi_hand_landmarks[0]
                 mp_draw.draw_landmarks(frame, hand_landmark, mp_hand.HAND_CONNECTIONS)
 
@@ -63,6 +70,8 @@ def video_stream():
                 landmark_17_horizontal = int(hand_landmark.landmark[17].x * frame.shape[1])
                 landmark_19_vertical = int(hand_landmark.landmark[19].y * frame.shape[0])
                 landmark_20_vertical = int(hand_landmark.landmark[20].y * frame.shape[0])
+
+                cursor_label.place(x=landmark_8_horizontal, y=landmark_8_vertical)
 
                 erasing_gesture = (
                     landmark_8_vertical < landmark_7_vertical
@@ -86,16 +95,13 @@ def video_stream():
                     and not erasing_gesture
                 )
 
-                cursor_label.place(x=landmark_8_horizontal, y=landmark_8_vertical)
-
                 if writing_gesture:
-                    global prev_x, prev_y
                     if prev_x is not None and prev_y is not None:
                         cv2.line(canvas, (prev_x, prev_y), (landmark_8_horizontal, landmark_8_vertical), (0, 0, 0), 5)
                     prev_x, prev_y = landmark_8_horizontal, landmark_8_vertical
                 else:
                     prev_x, prev_y = None, None
-
+                    
                 if erasing_gesture:
                     cv2.circle(canvas, (landmark_8_horizontal, landmark_8_vertical), 15, (255, 255, 255), -1)
                     prev_x, prev_y = None, None
@@ -107,12 +113,39 @@ def video_stream():
                 cursor_label.place(x=-20, y=-20)
 
             cv2.imshow("Frame", frame)
-            
+
             if cv2.waitKey(1) and not cv2.getWindowProperty("Frame", cv2.WND_PROP_VISIBLE):
                 break
 
     video.release()
     cv2.destroyAllWindows()
+
+def mouse_press(event):
+    global mouse_drawing, mouse_erasing, prev_x, prev_y
+    if event.num == 1:
+        if mouse_mode:
+            mouse_drawing = True
+            mouse_erasing = False
+            prev_x, prev_y = event.x, event.y
+    elif event.num == 3:
+        if mouse_mode:
+            mouse_drawing = False
+            mouse_erasing = True
+
+def mouse_release(event):
+    global mouse_drawing, mouse_erasing
+    mouse_drawing = False
+    mouse_erasing = False
+
+def mouse_motion(event):
+    global mouse_drawing, mouse_erasing, prev_x, prev_y
+    if mouse_mode:
+        if mouse_drawing:
+            if prev_x is not None and prev_y is not None:
+                cv2.line(canvas, (prev_x, prev_y), (event.x, event.y), (0, 0, 0), 5)
+            prev_x, prev_y = event.x, event.y
+        elif mouse_erasing:
+            cv2.circle(canvas, (event.x, event.y), 15, (255, 255, 255), -1)
 
 def save_image():
     file_path = filedialog.asksaveasfilename(defaultextension=".jpg", initialfile="image.jpg", filetypes=[("JPEG files", "*.jpg"), ("All files", "*.*")])
@@ -120,14 +153,21 @@ def save_image():
         img = Image.fromarray(canvas)
         img.save(file_path)
 
+def clear_canvas():
+    global canvas
+    canvas = np.ones((height, width, 3), dtype="uint8") * 255
+
 def convert_to_text():
     text_window = tk.Toplevel(root)
     text_window.title("Text Conversion Window")
     text_window.geometry("800x550")
 
-def clear_canvas():
-    global canvas
-    canvas = np.ones((height, width, 3), dtype="uint8") * 255
+def toggle_mode():
+    global video, mouse_mode, mouse_drawing, mouse_erasing, prev_x, prev_y
+    mouse_mode = not mouse_mode
+    mouse_drawing = False
+    mouse_erasing = False
+    prev_x, prev_y = None, None
 
 def close_app():
     video.release()
@@ -138,8 +178,13 @@ menu_frame = tk.Frame(root)
 menu_frame.pack(fill=tk.X)
 
 tk.Button(menu_frame, text="Save Image", command=save_image, bg="lightgray", fg="black").pack(side=tk.LEFT, padx=10, pady=3)
-tk.Button(menu_frame, text="Convert to Text", command=convert_to_text, bg="lightgray", fg="black").pack(side=tk.LEFT, padx=0, pady=3)
 tk.Button(menu_frame, text="Clear Canvas", command=clear_canvas, bg="lightgray", fg="black").pack(side=tk.LEFT, padx=10, pady=3)
+tk.Button(menu_frame, text="Convert to Text", command=convert_to_text, bg="lightgray", fg="black").pack(side=tk.LEFT, padx=10, pady=3)
+tk.Button(menu_frame, text="Toggle Mode", command=toggle_mode, bg="lightgray", fg="black").pack(side=tk.LEFT, padx=10, pady=3)
+
+canvas_label.bind("<ButtonPress>", mouse_press)
+canvas_label.bind("<ButtonRelease>", mouse_release)
+canvas_label.bind("<Motion>", mouse_motion)
 
 root.protocol("WM_DELETE_WINDOW", close_app)
 
